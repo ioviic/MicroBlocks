33fb55d73c7f64fe4abefee3ed4b75e8c6dd6048
ioviic <>
2018-03-04 23:19:04 +0200

    Add user Chip in Sidebar

--- src/app/blocks/sidebar/Sidebar.js
+++ src/app/blocks/sidebar/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SidebarLinks from './SidebarLinks';
 
+import { logoutAction } from '../login/actions';
+
 import { toggleHeader } from './actions';
 import type { State } from '../../../stateManagement/types/state';
 import { bindActionCreators } from 'redux';
@@ -9,12 +11,14 @@ import { connect } from 'react-redux';
 import SidebarConfig from './SidebarConfig';
 import injectConfigs from '../../../configurations/ConfigurationHOC';
 
-import { Drawer } from 'material-ui';
-import { withStyles } from 'material-ui/styles';
+import { Drawer, Avatar, ButtonBase } from 'material-ui';
 import image from '../../customization/sidebar-2.jpg';
+import { withStyles } from 'material-ui/styles';
 import { SideBarStyles as styles } from '../../customization/styles/Sidebar';
+
 type Props = {
   sidebar: *,
+  login: *,
   toggleHeader: () => mixed,
   classes: any,
 };
@@ -41,6 +45,15 @@ class Sidebar extends Component<Props & Configuration> {
                   Toggle Header
                 </button>
               </div>
+              {this.props.login.token &&
+                <ButtonBase className={classes.userChip} onClick={this.props.logoutAction}>
+                  <div className={classes.userInfo}>
+                    <span className={classes.userName}>{this.props.login.email}</span>
+                    <span className={classes.userEmail}>{this.props.login.email}</span>
+                  </div>
+                  <Avatar className={classes.userAvatar}>MB</Avatar>
+                </ButtonBase>
+              }
             </div>
 
             {image !== undefined && <div className={classes.background} style={{backgroundImage: "url("+image+")"}} />}
@@ -49,14 +62,15 @@ class Sidebar extends Component<Props & Configuration> {
     );
   }
 }
-const mapStateToProps = ({ sidebar }: State) => ({
-  sidebar,
+const mapStateToProps = ({ sidebar, login }: State) => ({
+  sidebar,login
 });
 
 const mapDispatchToProps = (dispatch: *) =>
   bindActionCreators(
     {
       toggleHeader,
+      logoutAction
     },
     dispatch
   );
