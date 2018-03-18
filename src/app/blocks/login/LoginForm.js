24834ec96a46a16b6b6277ff46e3bc816621e513
ioviic <>
2018-03-19 00:41:04 +0200

    Login Form StandardCard Using

--- src/app/blocks/login/LoginForm.js
+++ src/app/blocks/login/LoginForm.js
@@ -1,8 +1,9 @@
 // @flow
 import React, { Component } from 'react';
+import StandardCard from '../../components/StandardCard';
 import injectConfigs from '../../../configurations/ConfigurationHOC';
 import {
-  withStyles, Card, CardHeader, CardContent, FormControl, InputLabel, Input,Button, Grid
+  withStyles,FormControl, InputLabel, Input, Button, Grid
 } from 'material-ui';
 import { Clear } from 'material-ui-icons';
 // $FlowFixMe
@@ -15,7 +16,7 @@ import type { FormData } from './actions';
 type LoginState = {
   data: FormData;
   loading: boolean;
-  errors: FormData & {global: string};
+  errors: FormData & { global: string };
 }
 
 type Props = {
@@ -48,8 +49,8 @@ class LoginForm extends Component<Props & Configuration, LoginState> {
       this.setState({ loading: true });
       this.props.submit(this.state.data)
         .catch(err => {
-          this.setState({ errors: { ...errors, global: err.response.data.errors.global }, loading: false } );
-        } );
+          this.setState({ errors: { ...errors, global: err.response.data.errors.global }, loading: false });
+        });
     }
   };
 
@@ -62,33 +63,24 @@ class LoginForm extends Component<Props & Configuration, LoginState> {
     return errors;
   };
 
-  render () {
+  render() {
     const { data, errors } = this.state;
     const { classes } = this.props;
     return (
-      <Grid container className={classes.login}>
-        <Card className={classes.card}>
-          <CardHeader
-            classes={{
-              root: classes.cardHeader,
-              title: classes.cardTitle,
-              subheader: classes.cardSubtitle,
-            }}
-            title={'Login'}
-            subheader={'Please insert you mail and password'}
-          />
-          <CardContent>
-            <form onSubmit={this.onSubmit}>
-              <Grid container spacing={24}>
+      <StandardCard
+        title={'Login'}
+        subHeader={'Please insert you mail and password'}
+
+        content={
+          <form onSubmit={this.onSubmit}>
+            <Grid container spacing={24}>
               {errors.global && <InlineError className={classes.labelError} error={errors.global}/>}
               <Grid item lg={4} md={6} sm={12}>
                 <FormControl
                   error={!!errors.email}
                   classes={{ root: classes.formControl }}
                 >
-                  <InputLabel>
-                    {"Email"}
-                  </InputLabel>
+                  <InputLabel>{'Email'}</InputLabel>
                   <Input
                     classes={{
                       disabled: classes.disabled,
@@ -104,37 +96,40 @@ class LoginForm extends Component<Props & Configuration, LoginState> {
                   {errors.email && <InlineError className={classes.labelError} error={errors.email}/>}
                 </FormControl>
               </Grid>
-              <Grid  item lg={4} md={6} sm={12}>
-              <FormControl
-                error={!!errors.password}
-                classes={{ root: classes.formControl }}
-              >
-                <InputLabel>
-                  {"Password"}
-                </InputLabel>
-                <Input
-                  classes={{
-                    disabled: classes.disabled,
-                    underline: classes.underline,
-                  }}
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={data.password}
-                  onChange={this.onChange}
-                />
-                {errors.password && <InlineError className={classes.labelError} error={errors.password}/>}
-              </FormControl>
+              <Grid item lg={4} md={6} sm={12}>
+                <FormControl
+                  error={!!errors.password}
+                  classes={{ root: classes.formControl }}
+                >
+                  <InputLabel>{'Password'}</InputLabel>
+                  <Input
+                    classes={{
+                      disabled: classes.disabled,
+                      underline: classes.underline,
+                    }}
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={this.onChange}
+                  />
+                  {errors.password && <InlineError className={classes.labelError} error={errors.password}/>}
+                </FormControl>
               </Grid>
               <Grid item lg={4} md={6} sm={12}>
-                <Button type="submit" className={classes.button} variant="raised" onClick={this.onSubmit}> Login </Button>
+                <Button
+                  type="submit"
+                  className={classes.button}
+                  variant="raised"
+                  onClick={this.onSubmit}>
+                  Login
+                </Button>
               </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </Grid>
-    );
+            </Grid>
+          </form>
+        }
+        actions={null}
+      />);
   }
 }
 
