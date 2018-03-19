467739fac3f863ae4b60cf00be0b8e28dfe7fed8
ioviic <>
2018-03-19 23:14:35 +0200

    Add Table Component

---
+++ src/app/components/TableList.js
@@ -0,0 +1,64 @@
+// @flow
+import React, { Component } from 'react';
+
+import {withStyles, Table, TableBody, TableCell, TableHead, TableRow } from 'material-ui';
+
+import { StandardCardStyles as styles } from '../customization/styles/Components.js';
+
+type Props = {
+  headerTitles: Array<string> ,
+  tableRows: Array<Array<string>>,
+  classes: any,
+};
+
+class TableList extends Component<Props> {
+
+  render () {
+    const {headerTitles, tableRows, classes } = this.props;
+    return (
+      <Table className={classes.card}>
+        <TableHead>
+          <TableRow>
+            {
+              headerTitles.map((prop,key) => {
+                return (
+                  <TableCell
+                    className={classes.headerCell}
+                    key={key}>
+                    {prop}
+                  </TableCell>
+                );
+              })
+            }
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {
+            tableRows.map((prop,key) => {
+              return (
+                <TableRow
+                  className={classes.tableRow}
+                  key={key}
+                >
+                  {
+                    prop.map((prop,key) => {
+                      return (
+                        <TableCell
+                          className={classes.tableCell}
+                          key={key}>
+                          {prop}
+                        </TableCell>
+                      );
+                    })
+                  }
+                </TableRow>
+              );
+            })
+          }
+        </TableBody>
+      </Table>
+    );
+  }
+}
+
+export default withStyles(styles)(TableList);
