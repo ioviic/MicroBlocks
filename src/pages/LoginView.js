1c1cabc628c0c98c3fac8dc5781773954acc9254
ioviic <>
2018-09-16 22:40:33 +0300

    add onClick event for SidbarLinks in order to close the opened drawer

--- src/pages/LoginView.js
+++ src/pages/LoginView.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
 import { BlockComponent } from '../SDK';
 
 const styles = theme => ({ });
